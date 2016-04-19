f25d7d0be90feab9e9398c78e93d831c205fa7d3
Steven Atkinson <steven.atkinson@gmx.com>
2016-04-19 12:38:51 +0100

    Changed accessor on JsonPropertyExtractor class.

--- src/Nancy.Patch/PropertyExtractors/JsonPropertyExtractor.cs
+++ src/Nancy.Patch/PropertyExtractors/JsonPropertyExtractor.cs
@@ -5,7 +5,7 @@ using Newtonsoft.Json;
 
 namespace Nancy.Patch.PropertyExtractors
 {
-    public class JsonPropertyExtractor : IPropertyExtractor
+    class JsonPropertyExtractor : IPropertyExtractor
     {
         public IEnumerable<string> Extract(Stream requestStream)
         {
