b7a8d531a76c7c2e52ac2094805f98e580100905
Steven Atkinson <steven.atkinson@gmx.com>
2016-04-19 13:31:36 +0100

    Updated assembly info.

--- src/Nancy.Patch/Properties/AssemblyInfo.cs
+++ src/Nancy.Patch/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Nancy.Patch")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Added support for patching models")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Steven Atkinson")]
 [assembly: AssemblyProduct("Nancy.Patch")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 
 [assembly: InternalsVisibleTo("Nancy.Patch.Tests")]
